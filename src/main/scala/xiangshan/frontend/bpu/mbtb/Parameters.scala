45b1e437ebe89431aa0aae3d1307a01962620f63
edwu186 <wuchengkai1998@gmail.com>
2026-03-06 15:06:09 +0800

    feat(Turbo): 8 way Mbtb

--- src/main/scala/xiangshan/frontend/bpu/mbtb/Parameters.scala
+++ src/main/scala/xiangshan/frontend/bpu/mbtb/Parameters.scala
@@ -21,13 +21,14 @@ import xiangshan.frontend.bpu.HasBpuParameters
 
 case class MainBtbParameters(
     NumEntries: Int = 8192,
-    NumWay:     Int = 4,
-    // Lowest level banks used to resolve read-write conflicts and reduce SRAM power, each bank is a physical SRAM
+    NumWay:     Int = 8,
+    // Lowest level banks, each bank is a physical SRAM
+    // This banking is used to resolve read-write conflicts and reduce SRAM power
     NumInternalBanks: Int = 4,
     // NumAlignBanks is determined by top-level FetchBlockSize and FetchBlockAlignSize, not adjustable in mbtb
     TagWidth:        Int = 16,
     TargetWidth:     Int = 20,       // 2B aligned
-    WriteBufferSize: Int = 4,
+    WriteBufferSize: Int = 8,
     Replacer:        String = "Lru", // "Lru" or "Plru"
     // Base table
     TakenCntWidth: Int = 2,
