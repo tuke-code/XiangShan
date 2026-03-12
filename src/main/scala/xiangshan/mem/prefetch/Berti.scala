94dbe32d54209698a6655ab7300d0132d8b9dff7
Yanqin Li <liyanqin@bosc.ac.cn>
2026-03-13 02:39:54 +0800

    fix(berti): only train fire can be used to prefetch

--- src/main/scala/xiangshan/mem/prefetch/Berti.scala
+++ src/main/scala/xiangshan/mem/prefetch/Berti.scala
@@ -1012,19 +1012,19 @@ class BertiPrefetcher()(implicit p: Parameters) extends BasePrefecher with HasBe
   val prefetchBuffer = Module(new DeltaPrefetchBuffer(name, PREFETCH_FILTER_SIZE, PREFETCH_FILTER_SIZE))
 
   // 1. train filter
+  val trainValid = trainFilter.io.trainReq.valid
+  val trainFire = trainFilter.io.trainReq.fire
+  val trainBits = trainFilter.io.trainReq.bits
   val demandRefill = io.refillTrain.valid && isDemand(io.refillTrain.bits.metaSource)
+  val demandMiss = trainFire && trainBits.miss
+  val demandPfHit = trainFire && isFromL1Prefetch(trainBits.metaSource)
   trainFilter.io.enable := io.enable
   trainFilter.io.flush := false.B
   trainFilter.io.ldTrainOpt.map(_ := io.ld_in)
   trainFilter.io.stTrainOpt.map(_ := io.st_in)
-  trainFilter.io.trainReq.ready := !demandRefill
+  trainFilter.io.trainReq.ready := !demandRefill || trainBits.miss
 
   // 2. history table && delta
-  val trainValid = trainFilter.io.trainReq.valid
-  val trainBits = trainFilter.io.trainReq.bits
-  val demandMiss = trainValid && trainBits.miss
-  val demandPfHit = trainValid && isFromL1Prefetch(trainBits.metaSource)
-
   historyTable.io.access.valid := demandMiss || demandPfHit
   historyTable.io.access.bits.pc := trainBits.pc
   historyTable.io.access.bits.vaddr := trainBits.vaddr
