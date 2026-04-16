dfa191af75d14e708ff6eed7cf5aff81ae21ea8a
lewislzh <liuzehao19@mails.ucas.ac.cn>
2026-04-16 17:30:21 +0800

    feat(topdown): refine the sub-pipeline execution efficiency issue reason
    	Refine the sub-pipeline execution efficiency issue at the ROB head
    by dividing sub-pipeline execution stalls into those interrupted by cancellation
    (multi issue) and those caused by excessive execution latency after the final issue.

--- src/main/scala/xiangshan/backend/rob/RobBundles.scala
+++ src/main/scala/xiangshan/backend/rob/RobBundles.scala
@@ -93,7 +93,9 @@ object RobBundles extends HasCircularQueuePtrHelper {
     // debug_end
     // topdown
     val topdownIssued    = OptionWrapper(backendParams.debugEn, Bool())
+    val topdownCanceled  = OptionWrapper(backendParams.debugEn, Bool())
     val topdownIssueTime = OptionWrapper(backendParams.debugEn, UInt(XLEN.W))
+    val topdownLastIssueTime = OptionWrapper(backendParams.debugEn, UInt(XLEN.W))
 
     def isWritebacked: Bool = !uopNum.orR
     def isUopWritebacked: Bool = !uopNum.orR
@@ -171,7 +173,9 @@ object RobBundles extends HasCircularQueuePtrHelper {
       robEntry.debug_sim_trig.foreach(_ := debug.debug_sim_trig)
     }
     robEntry.topdownIssued.foreach(_ := false.B)
+    robEntry.topdownCanceled.foreach(_ := false.B)
     robEntry.topdownIssueTime.foreach(_ := 0.U)
+    robEntry.topdownLastIssueTime.foreach(_ := 0.U)
   }
 
   def connectCommitEntry(robCommitEntry: RobCommitEntryBundle, robEntry: RobEntryBundle): Unit = {
