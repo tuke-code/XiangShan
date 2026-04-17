5008194f9669323dcf2e2c788894a2b66e48ab6a
YuanDL <2524247987@qq.com>
2026-04-17 11:41:55 +0800

    feat(sc): change NumBanks to 4

--- src/main/scala/xiangshan/frontend/bpu/sc/Parameters.scala
+++ src/main/scala/xiangshan/frontend/bpu/sc/Parameters.scala
@@ -44,9 +44,9 @@ case class ScParameters(
     CtrWidth:            Int = 6,
     ThresholdWidth:      Int = 13,
     ThresholdInit:       Int = 1130, // magic number,greater than min and less than max
-    NumBanks:            Int = 2,
+    NumBanks:            Int = 4,
     WriteBufferSize:     Int = 4,
-    EnableScTrace:       Boolean = false
+    EnableScTrace:       Boolean = true
 ) {}
 
 trait HasScParameters extends HasBpuParameters {
