7571fe5bba301aa079083c73a7260b95d7693d50
Long Jiang <you@example.com>
2026-04-24 17:58:06 +0800

    timing(sbuffer):  register the cohTimeOutMask to get better timing

--- src/main/scala/xiangshan/mem/sbuffer/Sbuffer.scala
+++ src/main/scala/xiangshan/mem/sbuffer/Sbuffer.scala
@@ -284,7 +284,9 @@ class Sbuffer(implicit p: Parameters)
   // insert and merge: cohCount=0
   // every cycle cohCount+=1
   // if cohCount(EvictCountBits-1)==1, evict
-  val cohTimeOutMask = VecInit(widthMap(i => cohCount(i)(EvictCountBits-1) && stateVec(i).isActive()))
+  val cohTimeOutMask_wire = VecInit(widthMap(i => cohCount(i) >= io.csrCtrl.sbuffer_timeout && stateVec(i).isActive()))
+  val cohTimeOutMask = RegInit(VecInit(Seq.fill(StoreBufferSize)(false.B)))
+  cohTimeOutMask := cohTimeOutMask_wire
   val (cohTimeOutIdx, cohHasTimeOut) = PriorityEncoderWithFlag(cohTimeOutMask)
   val cohTimeOutOH = PriorityEncoderOH(cohTimeOutMask)
   val missqReplayTimeOutMask = VecInit(widthMap(i => missqReplayCount(i)(MissqReplayCountBits - 1) && stateVec(i).w_timeout))
