7a536b162a0fcf1b4b314ae75726218d3b63a8b1
Zhu Yu <yulightenyu@gmail.com>
2026-04-29 13:07:42 +0800

    fix: Add a parameter "txSourceReady" to control the Option IO LCrdy
    Add a parameter "txSourceReady" to control the Option IO LCrdy which
    allows AsyncBridge to correctly propagate back-pressure while maintaining
    copatibility with existing design
    
    Add low power handshake signals with Sink: QREQ/QACTIVE/QACCEPTn

--- src/main/scala/xiangshan/XSTileWrap.scala
+++ src/main/scala/xiangshan/XSTileWrap.scala
@@ -187,6 +187,9 @@ class XSTileWrap()(implicit p: Parameters) extends LazyModule
       case Some(param) =>
         val source = withClockAndReset(clock, reset_sync)(Module(new CHIAsyncBridgeSource(param)))
         source.io.enq <> tile.module.io.chi.get
+        tile.module.io.lcrdy.foreach { out =>
+          out := source.io.lcrdy
+        }
         io.chi <> source.io.async
       case None =>
         require(enableCHI)
